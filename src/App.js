8e486478e507e01e5fc18541f23570784cb81e7e
dotinto <corriolgamer@gmail.com>
2023-03-11 16:46:57 +0200

    Website React.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Links from './Links';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Links/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Links.js
@@ -0,0 +1,10 @@
+import './Links.css'
+
+function Links() {
+    return (
+        <div className="links-body">
+            <span className="links-title">Links:</span>
+        </div>
+    )
+}
+ export default Links;
