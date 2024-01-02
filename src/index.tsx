74d83f1799b93af9d36ec210918a40fdf65144fb
dotinto <corriolgamer@gmail.com>
2024-01-02 23:20:07 +0200

    .

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Discord = () => "dh=0d793f45c157b3e076524ade93f6ee2940926caa"
+
+export default function Dom() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<App />} />
+          <Route path="/.well-known/discord" element={<Discord />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<Dom />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
